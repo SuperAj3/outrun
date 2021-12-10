9be5b5a273a071f197f4b0eb69b04d09e9365a32
FairPlay137 <10647627+FairPlay137@users.noreply.github.com>
2021-12-10 01:47:26 -0800

    Update generic.go

--- requests/generic.go
+++ requests/generic.go
@@ -3,6 +3,6 @@ package requests
 type Base struct {
 	SessionID    string `json:"sessionId"`
 	Version      string `json:"version"`
-	RevivalVerID int64  `json:"revivalVerId,string,omitempty"`
+	RevivalVerID int64  `json:"revivalVerId,string"`
 	Seq          int64  `json:"seq,string"`
 }
