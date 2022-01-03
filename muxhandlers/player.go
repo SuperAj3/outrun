72ac6fbfbb4522cd571ad75a21c308aaa634f3f8
F121Live <55333985+F121Live@users.noreply.github.com>
2022-01-03 19:29:20 -0300

    Revert, also take out Gothic Amy

--- muxhandlers/player.go
+++ muxhandlers/player.go
@@ -93,17 +93,16 @@ func GetCharacterState(helper *helper.Helper) {
 	charindex = player.IndexOfChara(enums.CTStrHalloweenRouge)
 	if charindex == -1 {
 		player.CharacterState = append(player.CharacterState, netobj.DefaultRouletteOnlyLockedCharacter(constobjs.CharacterHalloweenRouge))
-	}*/
 	charindex := player.IndexOfChara(enums.CTStrHalloweenOmega)
 	if charindex == -1 {
 		player.CharacterState = append(player.CharacterState, netobj.DefaultRouletteOnlyLockedCharacter(constobjs.CharacterHalloweenOmega))
 	}
-	charindex = player.IndexOfChara(enums.CTStrGothicAmy)
+	charindex := player.IndexOfChara(enums.CTStrGothicAmy)
 	if charindex == -1 {
 		//helper.Out("Adding Gothic Amy to CharacterState")
 		player.CharacterState = append(player.CharacterState, netobj.DefaultSpecialLockedCharacter(constobjs.CharacterGothicAmy))
-	}
-	charindex = player.IndexOfChara(enums.CTStrMarine)
+	}*/
+	charindex := player.IndexOfChara(enums.CTStrMarine)
 	if charindex == -1 {
 		//helper.Out("Adding Marine to CharacterState")
 		player.CharacterState = append(player.CharacterState, netobj.DefaultRouletteOnlyLockedCharacter(constobjs.CharacterMarine))
