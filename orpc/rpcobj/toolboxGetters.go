2e8202eb5fd05c9989ff810d4344aed5cdd1bcf5
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-01-10 20:59:49 -0500

    add score handlers to orpc toolbox

--- orpc/rpcobj/toolboxGetters.go
+++ orpc/rpcobj/toolboxGetters.go
@@ -1,69 +1,93 @@
 package rpcobj
 
 import (
-    "strconv"
+	"strconv"
 	"strings"
 
-    "github.com/Mtbcooler/outrun/db"
+	"github.com/Mtbcooler/outrun/db"
 )
 
 func (t *Toolbox) GetUsername(uid string, reply *ToolboxReply) error {
-    player, err := db.GetPlayer(uid)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Info = "unable to get player: " + err.Error()
-        return err
-    }
-    reply.Status = StatusOK
-    reply.Info = player.Username
-    return nil
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = player.Username
+	return nil
 }
 
 func (t *Toolbox) GetRouletteTickets(uid string, reply *ToolboxReply) error {
-    player, err := db.GetPlayer(uid)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Info = "unable to get player: " + err.Error()
-        return err
-    }
-    reply.Status = StatusOK
-    reply.Info = strconv.Itoa(int(player.PlayerState.NumRouletteTicket))
-    return nil
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = strconv.Itoa(int(player.PlayerState.NumRouletteTicket))
+	return nil
 }
 
 func (t *Toolbox) GetLastLogin(uid string, reply *ToolboxReply) error {
-    player, err := db.GetPlayer(uid)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Info = "unable to get player: " + err.Error()
-        return err
-    }
-    reply.Status = StatusOK
-    reply.Info = strconv.Itoa(int(player.LastLogin))
-    return nil
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = strconv.Itoa(int(player.LastLogin))
+	return nil
 }
 
 func (t *Toolbox) GetCurrentTeam(uid string, reply *ToolboxReply) error {
-    player, err := db.GetPlayer(uid)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Info = "unable to get player: " + err.Error()
-        return err
-    }
-    result := []string{player.PlayerState.MainCharaID, player.PlayerState.SubCharaID}
-    reply.Status = StatusOK
-    reply.Info = strings.Join(result, ",") 
-    return nil
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	result := []string{player.PlayerState.MainCharaID, player.PlayerState.SubCharaID}
+	reply.Status = StatusOK
+	reply.Info = strings.Join(result, ",")
+	return nil
 }
 
 func (t *Toolbox) GetPersonalEvents(args ChangeValueArgs, reply *ToolboxValueReply) error {
-    player, err := db.GetPlayer(args.UID)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Result = "unable to get player: " + err.Error()
-        return err
-    }
-    reply.Status = StatusOK
-    reply.Result = player.PersonalEvents
-    return nil
+	player, err := db.GetPlayer(args.UID)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Result = "unable to get player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Result = player.PersonalEvents
+	return nil
+}
+
+func (t *Toolbox) GetStoryHighScore(uid string, reply *ToolboxReply) error {
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = strconv.Itoa(int(player.PlayerState.HighScore))
+	return nil
+}
+
+func (t *Toolbox) GetQuickHighScore(uid string, reply *ToolboxReply) error {
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = strconv.Itoa(int(player.PlayerState.TimedHighScore))
+	return nil
 }
