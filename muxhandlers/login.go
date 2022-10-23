fb2ed5ee1696980cc0883cc9708b665fb514cdcb
Ramen2X <64166386+Ramen2X@users.noreply.github.com>
2022-10-23 18:25:37 -0400

    update whitelist hack

--- muxhandlers/login.go
+++ muxhandlers/login.go
@@ -173,7 +173,7 @@ func Login(helper *helper.Helper) {
 				return
 			}
 			allowedToLogin := false
-			whitelistedIDs := []string{"5684022511", "7892875001"}
+			whitelistedIDs := []string{"5684022511", "7892875001", "1740310365"}
 			for _, idInList := range whitelistedIDs {
 			if uid == idInList { // this hack is terrible and should only be used in emergencies
 				allowedToLogin = true
