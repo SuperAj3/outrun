0fd61a47155e5ac6f7e442e75860894df17df147
FairPlay137 <10647627+FairPlay137@users.noreply.github.com>
2023-02-10 18:15:42 -0800

    Reduce maximum continues to 2 This is to improve the balance since there are competitive features in the Outrun for Revival codebase now.

--- netobj/playerVarious.go
+++ netobj/playerVarious.go
@@ -8,8 +8,8 @@ type PlayerVarious struct {
 	CmSkipCount          int64 `json:"cmSkipCount"` // no clear purpose
 	EnergyRecoveryMax    int64 `json:"energyRecoveryMax"`
 	EnergyRecoveryTime   int64 `json:"energyRecveryTime"`
-	OnePlayCmCount       int64 `json:"onePlayCmCount"`
-	OnePlayContinueCount int64 `json:"onePlayContinueCount"` // number of continues per run
+	OnePlayCmCount       int64 `json:"onePlayCmCount"`       // number of ad continues per run??? no effect in 2.x
+	OnePlayContinueCount int64 `json:"onePlayContinueCount"` // number of RSR continues per run
 	IsPurchased          int64 `json:"isPurchased"`          // removes ads? doesn't seem to do anything in 2.x
 }
 
@@ -18,7 +18,7 @@ func DefaultPlayerVarious() PlayerVarious {
 	energyRecoveryMax := gameconf.CFile.EnergyRecoveryMax
 	energyRecoveryTime := gameconf.CFile.EnergyRecoveryTime
 	onePlayCmCount := int64(0)
-	onePlayContinueCount := int64(5)
+	onePlayContinueCount := int64(2)
 	isPurchased := int64(0)
 	return PlayerVarious{
 		cmSkipCount,
