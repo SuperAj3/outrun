bb31db2ead139a0a5ed70841fb6a5a98fc5b881a
FairPlay137 <10647627+FairPlay137@users.noreply.github.com>
2024-03-24 13:34:29 -0700

    Begin work on ScheduledEventParser

--- config/scheduleconf/scheduledEventParser.go
+++ config/scheduleconf/scheduledEventParser.go
@@ -1,3 +1,26 @@
 package scheduleconf
 
+import "github.com/RunnersRevival/outrun/enums"
+
 // A Scheduled Event is a type of file which has all the necessary data for an event or campaign in a given timeslot.
+
+var TimeslotTypeMapping = map[string]int64{
+	"event":            0,
+	"campaign":         1,
+	"rouletteOddsOnly": 2,
+}
+
+var EventTypes = map[string]int64{
+	"specialStage":  enums.EventIDSpecialStage,
+	"raidBoss":      enums.EventIDRaidBoss,
+	"collectObject": enums.EventIDCollectObject,
+	"gacha":         enums.EventIDGacha,
+	"advert":        enums.EventIDAdvert,
+	"quick":         enums.EventIDQuick,
+	"bgm":           enums.EventIDBGM,
+}
+
+type ScheduledEventFile struct {
+	IsActive     bool   `json:"isActive,omitempty"`
+	TimeslotType string `json:"timeslotType,omitempty"`
+}
