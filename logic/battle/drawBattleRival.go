a0ca349ab4adc6606af4c7c2c3d018bdc4229e3f
F121Live <55333985+F121Live@users.noreply.github.com>
2025-01-20 19:04:23 -0300

    Battle: Actually make it 15 mil

--- logic/battle/drawBattleRival.go
+++ logic/battle/drawBattleRival.go
@@ -33,8 +33,8 @@ func DrawBattleRival(player netobj.Player, limit int) netobj.BattleState {
 						potentialRival.BattleState.ScoreRecordedToday &&
 						!potentialRival.BattleState.MatchedUpWithRival &&
 						currentTime < potentialRival.BattleState.BattleEndsAt &&
-						(potentialRival.BattleState.PrevDailyBattleHighScore < player.BattleState.PrevDailyBattleHighScore - 1500000) && 
-						potentialRival.BattleState.PrevDailyBattleHighScore > player.BattleState.PrevDailyBattleHighScore + 1500000{
+						(potentialRival.BattleState.PrevDailyBattleHighScore < player.BattleState.PrevDailyBattleHighScore - 15000000) && 
+						potentialRival.BattleState.PrevDailyBattleHighScore > player.BattleState.PrevDailyBattleHighScore + 15000000{
 						rivalID = playerIDs[index]
 						break
 					} else { 
